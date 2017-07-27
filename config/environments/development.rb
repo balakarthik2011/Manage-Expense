d52e37655dbd9fe3033959d6c021daa990f5e7ca
Bala Karthik <karthibala.m@gmail.com>
2017-07-27 23:50:09 +0530

    Nav bar added, Log out button  added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.smtp_settings = {
+      :user_name => 'app73171176@heroku.com',
+      :password => 'dbp4xloz9159',
+      :domain => 'yourdomain.com',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
